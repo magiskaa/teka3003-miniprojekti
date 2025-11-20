84f94bdaabffe2c6215177465f859a830dfad4a1
cL4ssiK <lassikupiainen2gmail.com>
2025-11-20 17:08:12 +0200

    Run skripti mac/linux

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+IMAGE_NAME="miniprojekti"
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+echo "Running Docker container..."
+docker run --rm --name miniprojekti-kontti $IMAGE_NAME "$@"
