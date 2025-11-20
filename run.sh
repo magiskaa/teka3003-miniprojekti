9dfb01cd488f9780bd1affafa7edf29a6acb36a5
cL4ssiK <lassikupiainen2gmail.com>
2025-11-20 17:25:05 +0200

    lisätään datahakemisto dockeriin siten että data säilyy kontti instanssien välissä

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ echo "Building Docker image..."
 docker build -t $IMAGE_NAME .
 
 echo "Running Docker container..."
-docker run --rm --name miniprojekti-kontti $IMAGE_NAME "$@"
+docker run --rm -v "$(pwd)/data:/app/data" --name miniprojekti-kontti $IMAGE_NAME "$@"
