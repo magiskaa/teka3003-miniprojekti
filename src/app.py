bb9de32d3d1e51170bfd79dd41a4b1659125ebb0
Valtteri Antikainen <vantikaine@gmail.com>
2025-11-23 17:02:49 +0200

    Lisätty app-luokan tynkä sekä console_io-luokka

---
+++ src/app.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+class App:
+    def __init__(self, io):
+        self.io = io
+
+    def connect_db(self):
+        db = sqlite3.connect("data/tietokanta.sqlite")
+        db.row_factory = sqlite3.Row
+        cursor = db.cursor()
+
+        cursor.execute(""" 
+            CREATE TABLE IF NOT EXISTS 
+            citations (
+                cite_key TEXT UNIQUE PRIMARY KEY,
+                author TEXT,
+                title TEXT,
+                year INTEGER
+            )
+        """)
+        db.commit()
+
+        return db, cursor
+
+    def run(self):
+        db, cursor = self.connect_db()
+
+        while True:
+            komento = self.io.read("\nKirjoita komento (esim. lisaa article): ")
+
+            if not komento:
+                break
+
+            if komento.startswith("lisaa"):
+                kom, arg = komento.split(" ")
+                self.io.write(f"\nKomento: {kom}")
+                self.io.write(f"Argumentti: {arg}")
+
+                # Esim.
+                # lisaa = Lisaa(kom, arg, db)
+                # lisaa.run() -> kysyy käyttäjältä author, title, year...
+
+                # Tietokantaan lisäys tässä vai Lisaa-luokassa?
+                # Esim.
+                # cursor.execute(""" 
+                #     INSERT INTO citations
+                #     VALUES (?,?,?,?)
+                # """, ("UE73N", "Matti Meikäläinen", "Matin Tarinat", 2012))
+                # db.commit()
+
+        db.close()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,10 @@
-import sqlite3
+from app import App
+from console_io import IO
+
+def main():
+    io = IO()
+    app = App(io)
+    app.run()
 
 if __name__ == "__main__":
-    # Luo tietokannan, Tee tämä app luokassa
-    conn = sqlite3.connect("data/tietokanta.sqlite")
-    cursor = conn.cursor()
-
-    # Esimerkkipöytä, myös app luokassa ja oikeilla jutuilla.
-    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)")
-    conn.commit()
-    
-    print("Hello world")
+    main()
